7adcc4774ee82b1acbbd4cf055b9164be292d346
Jason Kwong <skjKwong@gmail.com>
2024-04-14 00:06:25 -0700

    integrated clerk auth, login and sign up routing, styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from '@clerk/nextjs'
 
 const IBMPlex = IBM_Plex_Sans({
   subsets: ["latin"],
@@ -10,7 +11,7 @@ const IBMPlex = IBM_Plex_Sans({
 });
 
 export const metadata: Metadata = {
-  title: "AI Image Engine",
+  title: "AI ImageMaster",
   description: "AI-powered image generator",
 };
 
@@ -20,10 +21,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
-        {children}
-        </body>
-    </html>
+    <ClerkProvider appearance={{
+      variables: {colorPrimary: '#624cf5'}
+    }}>
+      <html lang="en">
+        <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+          {children}
+          </body>
+      </html>
+    </ClerkProvider>
   );
 }
