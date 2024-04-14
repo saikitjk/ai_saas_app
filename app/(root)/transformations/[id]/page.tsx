ac2c00b77582777e7b4373e7cc6315d27cc9a6fd
Jason Kwong <skjKwong@gmail.com>
2024-04-13 22:15:02 -0700

    setup file-system based routing

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const TransformationPage = () => {
+  return (
+    <div>TransformationPage</div>
+  )
+}
+
+export default TransformationPage
+
+
+// below is the example of extracting id from url and display on the ui
+
+// const TransformationPage = ({ params }:{params:{id : string}}) => {
+//   return (
+//     <div>TransformationPage {params.id}</div>
+
+//   )
+// }
